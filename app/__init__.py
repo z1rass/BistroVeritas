6cbc7ae8b5ac633244286c3fbb0022f02fb99d02
z1rass <kolgrast@outlook.com>
2024-01-07 23:09:43 +0100

    Google OAuth working

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,38 @@
-from flask import Flask, render_template, url_for
+import os
+import pathlib
+
+import requests
+from flask import Flask, session, abort, redirect, request, render_template, url_for
+from google.oauth2 import id_token
+from google_auth_oauthlib.flow import Flow
+from pip._vendor import cachecontrol
+import google.auth.transport.requests
+
+
+
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+GOOGLE_CLIENT_ID = "549086998736-neu819sgnn6aiu1hm2j30ui72fa41s76.apps.googleusercontent.com"
+client_secrets_file = os.path.join(pathlib.Path(__file__).parent, "client_secret.json")
+
+flow = Flow.from_client_secrets_file(
+    client_secrets_file=client_secrets_file,
+    scopes=["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email", "openid"],
+    redirect_uri="http://127.0.0.1:5000/callback"
+)
+
+
+def login_is_required(function):
+    def wrapper(*args, **kwargs):
+        if "google_id" not in session:
+            return abort(401)  # Authorization required
+        else:
+            return function()
+
+    return wrapper
+
 
 app = Flask(__name__)
+app.secret_key = "SuperPower2022"
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
-from app import app, render_template, url_for
-
+from app import app, render_template, url_for, abort, redirect, request, session
+from app import flow, google, requests, cachecontrol, id_token, GOOGLE_CLIENT_ID
 
 @app.route('/')
 def home():
@@ -12,3 +12,42 @@ def blog():
 @app.route('/menu')
 def menu():
     return render_template('menu.html')
+
+
+@app.route("/login")
+def login():
+    authorization_url, state = flow.authorization_url()
+    session["state"] = state
+    return redirect(authorization_url)
+
+
+@app.route("/callback")
+def callback():
+    flow.fetch_token(authorization_response=request.url)
+
+    if not session["state"] == request.args["state"]:
+        abort(500)  # State does not match!
+
+    credentials = flow.credentials
+    request_session = requests.session()
+    cached_session = cachecontrol.CacheControl(request_session)
+    token_request = google.auth.transport.requests.Request(session=cached_session)
+
+    id_info = id_token.verify_oauth2_token(
+        id_token=credentials._id_token,
+        request=token_request,
+        audience=GOOGLE_CLIENT_ID
+    )
+
+    session["google_id"] = id_info.get("sub")
+    session["name"] = id_info.get("name")
+    session['isAuthorised'] = True
+    name = session['name']
+    return render_template('home.html', name = name)
+
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/")
+
